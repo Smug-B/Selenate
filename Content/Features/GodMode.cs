68391efb6976bbed4ec87e888e50218002acfb81
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-15 15:52:14 -0500

    Basic God Mode
    The file structure is a bit icky. I might change it later.
    
    Totally not ripped from Cheat Sheet aha...

---
+++ Content/Features/GodMode.cs
@@ -0,0 +1,41 @@
+﻿using Selenate.Content.UI;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace Selenate.Content.Features
+{
+    public class GodMode : HotBarIcon
+    {
+        public static bool Enabled;
+
+        public GodMode() : base()
+        {
+        }
+
+        public override void LeftClick(UIMouseEvent evt)
+        {
+            base.LeftClick(evt);
+
+            Enabled = Toggle.Value;
+        }
+    }
+
+    public class GodModePlayer : ModPlayer
+    {
+        public override bool ImmuneTo(PlayerDeathReason damageSource, int cooldownCounter, bool dodgeable) =>
+            GodMode.Enabled ? true : base.ImmuneTo(damageSource, cooldownCounter, dodgeable);
+
+        public override void PreUpdate()
+        {
+            if (!GodMode.Enabled)
+            {
+                return;
+            }
+
+            Player.statLife = Player.statLifeMax2;
+            Player.statMana = Player.statManaMax2;
+            Player.wingTime = Player.wingTimeMax;
+        }
+    }
+}
