d311accb0f23c4e52c68000a47e7007ecff6d529
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-15 15:51:25 -0500

    Changed HotBarIcon Asset Lookup Location
    HotBarIcon now looks for the associate icon asset in its mirrored directory under Assets.

--- Content/UI/HotBarIcon.cs
+++ Content/UI/HotBarIcon.cs
@@ -7,12 +7,15 @@ namespace Selenate.Content.UI
 {
     public abstract class HotBarIcon : SUIImageButton, ILoadable
     {
+        // To-Do: Investigate if splitting and joining is faster than replacing twice. 
+        public string TexturePath => GetType().FullName.Replace('.', '/').Replace("Selenate/Content", "Assets");
+
         public IList<HotBarIcon> MutuallyExclusiveIcons;
 
         public HotBarIcon() : base(null)
         {
             // Icons are ideally 40x40.
-            Image = ModContent.Request<Texture2D>(GetType().FullName.Replace('.', '/'));
+            Image = Selenate.Request<Texture2D>(TexturePath);
             Width.Pixels = 44;
             Height.Pixels = 44;
         }
