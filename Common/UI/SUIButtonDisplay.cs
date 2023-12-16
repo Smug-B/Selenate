db46d27593d0ac014e7df0464df6e234c0b56a53
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-15 22:00:18 -0500

    Create SUIButtonDisplay.cs
    SUIButtonDisplay is a UIList wrapper that allows for horizontal element growth on top of vertical element growth.
    
    Horizontal scrollbar support may or may not be added in the future, depending on our need for it.
    
    Currently, it has only been primitively tested to be functional with limitless horizontal growth.

---
+++ Common/UI/SUIButtonDisplay.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Reflection;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+
+namespace Selenate.Common.UI
+{
+    public class SUIButtonDisplay : UIList
+    {
+        public static readonly FieldInfo InnerListHeight = typeof(UIList).GetField("_innerListHeight", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public int MaxAxialElements
+        {
+            get => maxAxialElements;
+            set
+            {
+                maxAxialElements = value;
+                Recalculate();
+            }
+        }
+
+        private int maxAxialElements;
+
+        public bool PrioritizeVerticalGrowth
+        {
+            get => prioritizeVerticalGrowth;
+            set
+            {
+                prioritizeVerticalGrowth = value;
+                Recalculate();
+            }
+        }
+
+        private bool prioritizeVerticalGrowth;
+
+        // Inconsistent with Vanilla's UIList but God, that's garbage.
+        public float InnerListWidth { get; protected set; }
+
+        public SUIButtonDisplay(int allottedAxialElements = -1) : base()
+        {
+            maxAxialElements = allottedAxialElements;
+        }
+
+        public override void RecalculateChildren()
+        {
+            base.RecalculateChildren();
+
+            Vector2 inner = Vector2.Zero;
+            Vector2 max = Vector2.Zero;
+            float padding = _items.Count == 1 ? 0f : ListPadding;
+
+            int totalElements = 0;
+            int maxAxialElements = MaxAxialElements >= 0 ? MaxAxialElements : _items.Count;
+            while (true)
+            {
+                float largestGrowth = 0;
+                for (int i = 0; i < maxAxialElements; i++)
+                {
+                    UIElement currentElement = _items[totalElements];
+                    currentElement.Left.Set(inner.X, 0);
+                    currentElement.Top.Set(inner.Y, 0);
+                    currentElement.Recalculate();
+
+                    float elementWidth = currentElement.GetOuterDimensions().Width;
+                    float elementHeight = currentElement.GetOuterDimensions().Height;
+                    float widthGrowth = inner.X + elementWidth + padding;
+                    float heightGrowth = inner.Y + elementHeight + padding;
+
+                    max.X = Math.Max(max.X, widthGrowth);
+                    max.Y = Math.Max(max.Y, heightGrowth);
+
+                    float consideredAxis = PrioritizeVerticalGrowth ? elementWidth : elementHeight;
+                    if (largestGrowth < consideredAxis)
+                    {
+                        largestGrowth = consideredAxis;
+                    }
+
+                    if (PrioritizeVerticalGrowth)
+                    {
+                        inner.Y = heightGrowth;
+                    }
+                    else
+                    {
+                        inner.X += elementWidth + padding;
+                    }
+
+                    if (++totalElements >= _items.Count)
+                    {
+                        InnerListWidth = max.X - padding;
+                        InnerListHeight.SetValue(this, max.Y - padding);
+                        return;
+                    }
+                }
+
+                if (PrioritizeVerticalGrowth)
+                {
+                    inner.X += largestGrowth + padding;
+                    inner.Y = 0;
+                }
+                else
+                {
+                    inner.X = 0;
+                    inner.Y += largestGrowth + padding;
+                }
+            }
+        }
+    }
+}
