643e09d5c33986022d8e653e1e78b39f19895f98
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-16 00:46:48 -0500

    Updated HotBar for Sub-HotBar Functionality
    A bit of gross code that will need clean-up later :tm:.
    
    HotBar is no longer a panel but rather an element containing two hot bars: the main hot bar and a sub hot bar whose contents are defined by the main hot bar.
    
    To Do:
    - Add cleaner transitions for changing secondary hot bar elements.
    - Reduce instances of magic number.
    - Add functionality beyond the current implementation. (I.e. beyond being anchored at the bottom of the screen)

--- Content/UI/HotBar.cs
+++ Content/UI/HotBar.cs
@@ -1,28 +1,87 @@
-﻿using Selenate.Common.UI;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Selenate.Common.UI;
+using System;
 using System.Collections.Generic;
+using Terraria.UI;
 
 namespace Selenate.Content.UI
 {
-    public class HotBar : SUIPanel
+    public class HotBar : UIElement
     {
-        public static IList<HotBarIcon> LoadedFeatures { get; set; } = new List<HotBarIcon>();
+        public static IList<HotBarIcon> LoadedFeatures { get; private set; } = new List<HotBarIcon>();
 
-        public override void OnInitialize()
+        public SUIPanel MainBackground { get; private set; }
+
+        public SUIButtonDisplay MainHotBar { get; private set; }
+
+        protected SUIPanel SecondaryBackground { get; private set; }
+
+        protected SUIButtonDisplay SecondaryHotBar { get; set; }
+
+        public bool SecondaryHotBarActive { get; set; }
+
+        public float SecondaryHotBarTransition
         {
-            SUIButtonDisplay mainHotBar = new SUIButtonDisplay();
+            get => secondaryHotBarTransition;
+            set => secondaryHotBarTransition = Math.Clamp(value, 0f, 1f);
+        }
 
+        private float secondaryHotBarTransition;
+
+        public float SecondaryHotBarTransitionValue { get; set; } = 0.1f;
+
+        public override async void OnInitialize()
+        {
+            SecondaryBackground = new SUIPanel();
+            SecondaryBackground.Top.Pixels = 10;
+            SecondaryHotBar = new SUIButtonDisplay();
+            SecondaryBackground.VAlign = 1f;
+            SecondaryBackground.Append(SecondaryHotBar);
+            Append(SecondaryBackground);
+
+            MainBackground = new SUIPanel();
+            MainBackground.VAlign = 1f;
+            MainHotBar = new SUIButtonDisplay();
+            PopulateHotBar(MainHotBar, LoadedFeatures, out float width, out float height);
+            MainBackground.Width.Set(MainBackground.PaddingLeft + MainBackground.PaddingRight + width, 0);
+            MainBackground.Height.Set(MainBackground.PaddingTop + MainBackground.PaddingBottom + height, 0);
+            MainBackground.Append(MainHotBar);
+            Append(MainBackground);
+
+            Width.Pixels = MainBackground.Width.Pixels;
+            Height.Pixels = MainBackground.Height.Pixels * 2 + 10;
+        }
+
+        protected void PopulateHotBar(SUIButtonDisplay hotBar, IList<HotBarIcon> hotBarIcons, out float width, out float height, int constructionSize = 100)
+        {
+            hotBar.Clear();
             // Stinky code notice: these values are throwaways because they're needed to properly calculate the inner dimensions.
-            mainHotBar.Width.Pixels = LoadedFeatures.Count * 100;
-            mainHotBar.Height.Pixels = 100;
+            hotBar.Width.Pixels = hotBarIcons.Count * constructionSize;
+            hotBar.Height.Pixels = constructionSize;
+            hotBar.AddRange(hotBarIcons);
+            hotBar.Recalculate();
+            width = hotBar.InnerListWidth;
+            height = hotBar.GetTotalHeight();
+        }
 
-            mainHotBar.AddRange(LoadedFeatures);
-            mainHotBar.Recalculate();
-            float width = mainHotBar.InnerListWidth;
-            float height = mainHotBar.GetTotalHeight();
-            Append(mainHotBar);
+        public virtual void PopulateSecondaryHotBar(IList<HotBarIcon> hotBarIcons, int constructionSize = 100)
+        {
+            SecondaryBackground.Width.Pixels = hotBarIcons.Count * constructionSize;
+            SecondaryBackground.Height.Pixels = constructionSize;
+            SecondaryBackground.Recalculate();
+            PopulateHotBar(SecondaryHotBar, hotBarIcons, out float width, out float height, constructionSize);
+            SecondaryBackground.Width.Set(SecondaryBackground.PaddingLeft + SecondaryBackground.PaddingRight + width, 0);
+            SecondaryBackground.Height.Set(SecondaryBackground.PaddingTop + SecondaryBackground.PaddingBottom + height, 0);
+        }
 
-            Width.Pixels = PaddingLeft + PaddingRight + width;
-            Height.Pixels = height + PaddingTop + PaddingBottom;
+        public override void Update(GameTime gameTime)
+        {
+            SecondaryHotBarTransition += SecondaryHotBarActive ? SecondaryHotBarTransitionValue : -SecondaryHotBarTransitionValue;
+
+            SecondaryBackground.Top.Pixels = MainBackground.Height.Pixels + 10 - (MainBackground.Height.Pixels + SecondaryBackground.Height.Pixels + 10) * SecondaryHotBarTransition;
+            // Recalculate();
+            base.Update(gameTime);
         }
     }
 }
