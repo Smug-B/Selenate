a0d1ecc9f6d10210b932f7adb75340e54273af16
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-15 22:04:21 -0500

    Browser
    Browser is planned to be the main hot bar icon that extends to reveal all the 'browser' tool icons, e.g. item browser, npc browser, recipe browser.

---
+++ Content/Features/Browser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Selenate.Content.UI;
+using Terraria;
+
+namespace Selenate.Content.Features
+{
+    public class Browser : HotBarIcon
+    {
+        public Browser() : base()
+        {
+        }
+    }
+}
