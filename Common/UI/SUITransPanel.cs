bbce462ed508352752ded15dfe79ef30553cb418
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-19 19:29:14 -0500

    SUITransPanel
    Moved transition logic employed with SecondaryHotBar into its own element: SUITransPanel.
    
    The element will likely be expanded in the future, but for now it's just a simple lerp.

---
+++ Common/UI/SUITransPanel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria.UI;
+
+namespace Selenate.Common.UI
+{
+    public class SUITransPanel : SUIPanel
+    {
+        public Vector2 ToPosPixel { get; set; }
+
+        public Vector2 FromPosPixel { get; set; }
+
+        public bool Active { get; set; }
+
+        public float TransProgress
+        {
+            get => transProgress;
+            set => transProgress = Math.Clamp(value, 0f, 1f);
+        }
+
+        private float transProgress;
+
+        public float TransValue { get; set; } = 0.1f;
+
+        public SUITransPanel(Vector2 toPos, Vector2 fromPos)
+        {
+            ToPosPixel = toPos;
+            FromPosPixel = fromPos;
+            Left.Pixels = FromPosPixel.X;
+            Top.Pixels = FromPosPixel.Y;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            TransProgress += Active ? TransValue : -TransValue;
+
+            Vector2 posDifference = ToPosPixel - FromPosPixel;
+            Left.Pixels = FromPosPixel.X + posDifference.X * TransProgress;
+            Top.Pixels = FromPosPixel.Y + posDifference.Y * TransProgress;
+
+            base.Update(gameTime);
+        }
+    }
+}
--- Content/UI/HotBar.cs
+++ Content/UI/HotBar.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Selenate.Common.UI;
 using System;
 using System.Collections.Generic;
@@ -15,28 +14,18 @@ namespace Selenate.Content.UI
 
         public SUIButtonDisplay MainHotBar { get; private set; }
 
-        protected SUIPanel SecondaryBackground { get; private set; }
+        protected SUITransPanel SecondaryBackground { get; private set; }
 
         protected SUIButtonDisplay SecondaryHotBar { get; set; }
 
-        public bool SecondaryHotBarActive { get; set; }
+        public bool SecondaryHotBarActive { get => SecondaryBackground.Active; set => SecondaryBackground.Active = value; }
 
-        public float SecondaryHotBarTransition
+        public override void OnInitialize()
         {
-            get => secondaryHotBarTransition;
-            set => secondaryHotBarTransition = Math.Clamp(value, 0f, 1f);
-        }
-
-        private float secondaryHotBarTransition;
-
-        public float SecondaryHotBarTransitionValue { get; set; } = 0.1f;
-
-        public override async void OnInitialize()
-        {
-            SecondaryBackground = new SUIPanel();
-            SecondaryBackground.Top.Pixels = 10;
-            SecondaryHotBar = new SUIButtonDisplay();
+            SecondaryBackground = new SUITransPanel(Vector2.Zero, new Vector2(0, 100));
+            SecondaryBackground.HAlign = 0.5f;
             SecondaryBackground.VAlign = 1f;
+            SecondaryHotBar = new SUIButtonDisplay();
             SecondaryBackground.Append(SecondaryHotBar);
             Append(SecondaryBackground);
 
@@ -73,13 +62,12 @@ namespace Selenate.Content.UI
             PopulateHotBar(SecondaryHotBar, hotBarIcons, out float width, out float height, constructionSize);
             SecondaryBackground.Width.Set(SecondaryBackground.PaddingLeft + SecondaryBackground.PaddingRight + width, 0);
             SecondaryBackground.Height.Set(SecondaryBackground.PaddingTop + SecondaryBackground.PaddingBottom + height, 0);
+            SecondaryBackground.ToPosPixel = new Vector2(0, -MainBackground.Height.Pixels);
+            SecondaryBackground.Visible = true;
         }
 
         public override void Update(GameTime gameTime)
         {
-            SecondaryHotBarTransition += SecondaryHotBarActive ? SecondaryHotBarTransitionValue : -SecondaryHotBarTransitionValue;
-
-            SecondaryBackground.Top.Pixels = MainBackground.Height.Pixels + 10 - (MainBackground.Height.Pixels + SecondaryBackground.Height.Pixels + 10) * SecondaryHotBarTransition;
             // Recalculate();
             base.Update(gameTime);
         }
