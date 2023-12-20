07b606d97b9a2b675377d47934953f65582b7732
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-19 19:27:12 -0500

    SUIPanel Visibility
    SUIPanels have a visibility boolean that dictates whether or not the element is drawn and whether the player can interact with the element.

--- Common/UI/SUIPanel.cs
+++ Common/UI/SUIPanel.cs
@@ -5,18 +5,41 @@ using Terraria.GameContent.UI.Elements;
 
 namespace Selenate.Common.UI
 {
-	public class SUIPanel : UIPanel
-	{
-		public SUIPanel() : 
-			base(Selenate.Request<Texture2D>("Assets/UI/PanelBackground"), Selenate.Request<Texture2D>("Assets/UI/PanelBorder")) => DefaultColors();
+    public class SUIPanel : UIPanel
+    {
+        // To-Do: Make visibility impact whether or not this element can be interacted with, but better.
+        public bool Visible
+        {
+            get => visible;
+            set
+            {
+                visible = value;
+                IgnoresMouseInteraction = !visible;
+            }
+        }
 
-		public SUIPanel(Asset<Texture2D> background, Asset<Texture2D> border, int cornerSize = 12, int barSize = 4) :
-			base(background, border, cornerSize, barSize) => DefaultColors();
+        private bool visible = true;
 
-		public void DefaultColors()
-		{
-			BorderColor = UIPalette.PanelBorder;
-			BackgroundColor = UIPalette.PanelBackground;
-		}
-	}
+        public SUIPanel() :
+            base(Selenate.Request<Texture2D>("Assets/UI/PanelBackground"), Selenate.Request<Texture2D>("Assets/UI/PanelBorder")) => DefaultColors();
+
+        public SUIPanel(Asset<Texture2D> background, Asset<Texture2D> border, int cornerSize = 12, int barSize = 4) :
+            base(background, border, cornerSize, barSize) => DefaultColors();
+
+        public void DefaultColors()
+        {
+            BorderColor = UIPalette.PanelBorder;
+            BackgroundColor = UIPalette.PanelBackground;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!Visible)
+            {
+                return;
+            }
+
+            base.Draw(spriteBatch);
+        }
+    }
 }
