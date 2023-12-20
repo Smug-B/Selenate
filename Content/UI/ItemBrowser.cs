70bdd98a8265e629601e0d07e42a50e8a7567976
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-12-19 23:07:32 -0500

    Oh my God, this is horrible.
    But I have to leave soon for break so!
    
    To-Do: Literally everything.

---
+++ Content/UI/ItemBrowser.cs
@@ -0,0 +1,63 @@
+﻿using ReLogic.Threading;
+using Selenate.Common.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace Selenate.Content.UI
+{
+    public class ItemBrowser : UIPanel
+    {
+        public SUIButtonDisplay Items { get; set; }
+
+        public int HorizontalElementLimit
+        {
+            get => horizontalElementLimit;
+            set
+            {
+                horizontalElementLimit = value;
+            }
+        }
+
+        private int horizontalElementLimit = 10;
+
+        public override void OnInitialize()
+        {
+            Items = new SUIButtonDisplay(HorizontalElementLimit);
+            Items.Top.Pixels = 100;
+            List<ItemButton> allItems = new List<ItemButton>();
+            void loadItem(int from, int to, object context)
+            {
+                for (int i = from; i < to; i++)
+                {
+                    ItemButton itemButton = new ItemButton(i);
+                    itemButton.Width.Pixels = 40;
+                    itemButton.Height.Pixels = 40;
+                    allItems.Add(itemButton);
+                }
+            }
+            FastParallel.For(0, ItemID.Count - 1, loadItem);
+            allItems.Sort();
+            Items.AddRange(allItems);
+            Items.Width.Pixels = 500;
+            Items.Height.Pixels = 500;
+
+            FixedUIScrollbar DescriptionScrollbar = new FixedUIScrollbar(UserInterface.ActiveInstance);
+            DescriptionScrollbar.Left.Set(4, 0f);
+            DescriptionScrollbar.Height.Set(0, 1f);
+            DescriptionScrollbar.HAlign = 1f;
+            Items.Append(DescriptionScrollbar);
+            Items.SetScrollbar(DescriptionScrollbar);
+
+            Width.Pixels = 500;
+            Height.Pixels = 650;
+            Append(Items);
+        }
+    }
+}
---
+++ Content/UI/ItemButton.cs
@@ -0,0 +1,38 @@
+﻿using Selenate.Common.UI;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.UI;
+
+namespace Selenate.Content.UI
+{
+    public class ItemButton : SUIImageButton
+    {
+        public int ItemID { get; }
+
+        public Item Item { get; private set; }
+
+        public ItemButton(int itemID) : base(null, false)
+        {
+            ItemID = itemID;
+            Main.instance.LoadItem(itemID);
+            Image = TextureAssets.Item[itemID];
+            Item = new Item(itemID);
+        }
+
+        public override void MouseOver(UIMouseEvent evt)
+        {
+            base.MouseOver(evt);
+        }
+
+        public override int CompareTo(object obj)
+        {
+            if (obj is not ItemButton itemButton || ItemID == itemButton.ItemID)
+            {
+                return 0;
+            }
+
+            return ItemID > itemButton.ItemID ? 1 : -1;
+        }
+    }
+}
